d2177cf12bb5c3809e13331d58889efd8689bead
Grigory <grigory.chaykovsky@gmail.com>
2026-02-28 13:47:05 +0300

    fix loss logging

--- src/train_routine.py
+++ src/train_routine.py
@@ -31,6 +31,9 @@ from tqdm import tqdm
 def train_epoch(model, loader, criterion, optimizer, device : Union[torch.device, str] ="cpu"):
     model.train()
     model.to(device)
+    total_loss = 0
+    num_tokens = 0
+
     for de, de_lengths, en, en_lenghts in tqdm(loader):
         de_tokens = de[:, :de_lengths.max()].to(device)
         en_tokens = en[:, :en_lenghts.max()].to(device)
@@ -41,20 +44,30 @@ def train_epoch(model, loader, criterion, optimizer, device : Union[torch.device
         loss.backward()
         optimizer.step()
 
-    return loss
+        total_loss += loss.item() * len(en_tokens)
+        num_tokens += len(en_tokens)
+
+    return total_loss / num_tokens
 
 
 @torch.no_grad()
 def validate_epoch(model, loader, criterion, device : Union[torch.device, str] ="cpu"):
     model.eval()
     model.to(device)
+    total_loss = 0
+    num_tokens = 0
+
     for de, de_lengths, en, en_lenghts in tqdm(loader):
         de_tokens = de[:, :de_lengths.max()].to(device)
         en_tokens = en[:, :en_lenghts.max()].to(device)
         logits = model(de_tokens, en_tokens[:, :-1])
         loss = criterion(logits.permute(0, 2, 1), en_tokens[:, 1:])
 
-    return loss
+        total_loss += loss.item() * len(en_tokens)
+        num_tokens += len(en_tokens)
+
+    return total_loss / num_tokens
+
 
 def train(model, train_loader, val_loader, optimizer, scheduler, criterion, n_epochs : int = 1, device : Union[torch.device, str] = "cpu"):
     for i in range(1, n_epochs + 1):
