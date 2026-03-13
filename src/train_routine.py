da87477b7f8115f4d9715be59f696c8915734751
Grigory <grigory.chaykovsky@gmail.com>
2026-03-13 11:05:45 +0300

    tf 1

--- src/train_routine.py
+++ src/train_routine.py
@@ -7,6 +7,37 @@ warnings.filterwarnings("ignore")
 import torch
 
 
+def create_decoder_causal_mask(batch_size, seq_len):
+    mask = torch.triu(torch.ones(batch_size, seq_len, seq_len), diagonal=1).type(torch.int)
+    return mask == 0
+
+def collate_fn(batch, device = torch.device("cuda")):
+    src = torch.cat(list(map(lambda x : x[0].unsqueeze(0), batch)))
+    src_lens = torch.cat(list(map(lambda x : x[1].unsqueeze(0), batch)))
+    src = src[:, :src_lens.max()]
+    src_mask = (src != 1).unsqueeze(1).unsqueeze(1)
+    if len(batch[0]) == 2:
+        return {
+            "src" : src,
+            "src_mask" : src_mask
+        }
+
+    tgt = torch.cat(list(map(lambda x : x[2].unsqueeze(0), batch)))
+    tgt_lens = torch.cat(list(map(lambda x : x[3].unsqueeze(0), batch)))
+    tgt = tgt[:, :tgt_lens.max() - 1]
+    labels = torch.cat(list(map(lambda x : x[2].unsqueeze(0), batch)))
+    labels = labels[:, 1:tgt_lens.max()]
+    tgt_mask = ((tgt != 1) & (tgt != 3)).unsqueeze(1) & create_decoder_causal_mask(*tgt.shape).to(device)
+    tgt_mask = tgt_mask.unsqueeze(1)
+    return {
+        "src" : src,
+        "src_mask" : src_mask,
+        "tgt" : tgt,
+        "tgt_mask" : tgt_mask,
+        "label" : labels
+    }
+
+
 def create_dataset(split : str, path_to_data="../data", device=torch.device("cpu"), en_vocab=None, de_vocab=None):
     de = "{}/{}.de-en.de".format(path_to_data, split)
     de_dataset = BHW2Dataset(de, device=device, vocab=de_vocab)
@@ -16,6 +47,16 @@ def create_dataset(split : str, path_to_data="../data", device=torch.device("cpu
     en = "{}/{}.de-en.en".format(path_to_data, split)
     en_dataset = BHW2Dataset(en, device=device, vocab=en_vocab)
     return BHW2Allin1Dataset(de_dataset, en_dataset)
+
+
+def create_dataloaders_tf(path_to_data="../data", batch_size=32, device=torch.device("cpu")):
+    train_set = create_dataset("train", path_to_data=path_to_data, device=device)
+    val_set = create_dataset("val", path_to_data=path_to_data, device=device, en_vocab=train_set.en.vocab, de_vocab=train_set.de.vocab)
+    test_set = create_dataset("test1", path_to_data=path_to_data, device=device, en_vocab=train_set.en.vocab, de_vocab=train_set.de.vocab)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=False, collate_fn=lambda batch : collate_fn(batch, device=device))
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=0, pin_memory=False, collate_fn=lambda batch : collate_fn(batch, device=device))
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=0, pin_memory=False, collate_fn=lambda batch : collate_fn(batch, device=device))
+    return train_loader, val_loader, test_loader
 
 
 def create_dataloaders(path_to_data="../data", batch_size=32, device=torch.device("cpu")):
